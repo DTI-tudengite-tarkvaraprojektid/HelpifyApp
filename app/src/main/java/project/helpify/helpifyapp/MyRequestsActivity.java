639f9666c8d2fa49fcc6ab6c20f50bdb0884f0af
Mariam Reintop <mreintop@tlu.ee>
2017-06-20 11:26:43 +0300

    Error messages on log in and registration pages

--- app/src/main/java/project/helpify/helpifyapp/MyRequestsActivity.java
+++ app/src/main/java/project/helpify/helpifyapp/MyRequestsActivity.java
@@ -21,8 +21,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -226,7 +230,28 @@ public class MyRequestsActivity extends AppCompatActivity implements View.OnClic
             }
         });
 
-        //**
+        String user = firebaseAuth.getCurrentUser().getUid();
+        mDatabase.child("quests").child(user).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+
+                   for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    String qName = (String) snapshot.child("name").getValue();
+                    String qQuest = (String) snapshot.child("quest").getValue();
+                    String qDate = (String) snapshot.child("endDate").getValue();
+
+                       System.out.println(qName+" "+qQuest+" "+qDate);
+
+                }}
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
          buttonSaveUserData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -241,6 +266,8 @@ public class MyRequestsActivity extends AppCompatActivity implements View.OnClic
                     String userId = firebaseAuth.getCurrentUser().getUid();
                     String userEmail = firebaseAuth.getCurrentUser().getEmail();
                     mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
 
                     //http://tutorials.jenkov.com/java-internationalization/simpledateformat.html
                     String date_pattern = "dd/MM/yyyy HH:mm";
